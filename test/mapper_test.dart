54b9b1728d8fe5d5156612e9e102b2c45008d1d8
RagehAz <rageh.az@gmail.com>
2023-01-12 14:19:25 +0200

    - added three old method tests

--- test/mapper_test.dart
+++ test/mapper_test.dart
@@ -631,5 +631,73 @@ void main() {
     ]);
   });
   // -----------------------------------------------------------------------------
-  // void f() {}
+
+  /// CLEANING
+
+  // --------------------
+  const Map<String, dynamic> _mapXXX = {
+    'id': 'id',
+    'name': 'name',
+    'phid': 'phid',
+    'phidType': 'phidType',
+    'thing': 0,
+  };
+  // --------------------
+  test('cleanZeroValuesPairs', () {
+
+    final Map<String, dynamic> _cleaned = Mapper.cleanZeroValuesPairs(
+      map: _mapXXX,
+    );
+
+    const Map<String, dynamic> _expected = {
+      'id': 'id',
+      'name': 'name',
+      'phid': 'phid',
+      'phidType': 'phidType',
+    };
+
+    expect(_cleaned, _expected);
+
+  });
+  // --------------------
+  test('removePair', () {
+
+    final Map<String, dynamic> _a = Mapper.removePair(
+      map: {},
+      fieldKey: 'id',
+    );
+    expect(_a, {});
+
+    final Map<String, dynamic> _b = Mapper.removePair(
+      map: null,
+      fieldKey: 'id',
+    );
+    expect(_b, {});
+
+
+    final Map<String, dynamic> _c = Mapper.removePair(
+      map: {'id' : 'x'},
+      fieldKey: 'id',
+    );
+    expect(_c, {});
+
+    final Map<String, dynamic> _d = Mapper.removePair(
+      map: _mapXXX,
+      fieldKey: 'id',
+    );
+
+    const Map<String, dynamic> _expected = {
+      'name': 'name',
+      'phid': 'phid',
+      'phidType': 'phidType',
+      'thing': 0,
+    };
+
+    expect(_d, _expected);
+
+
+
+  });
+  // -----------------------------------------------------------------------------
+  void f() {}
 }
